4281b16b67a09d91192ee5ed545c5bba96c04976
Cordell <cordellbonnieux@gmail.com>
2021-07-26 21:19:50 -0700

    Created randomSearch()

--- Compare.java
+++ Compare.java
@@ -10,14 +10,16 @@ public class Compare {
 
             int [] list = genArray(size); 
 
-            int avgLnS = linearSearch(list);
+            int num = randomSearch(list);
 
-            int avgBS = binarySearch(list);
+            int avgLnS = linearSearch(list, num);
+
+            int avgBS = binarySearch(list, num);
 
             // develop the code to find out the average comparisons for
             // searching a target in the list using linear search and binary search
 
-            System.out.printf "%-10d%-20.2f%-20.2f\n", size, avgLnS, avgBS);
+            System.out.printf("%-10d%-20.2f%-20.2f\n", size, avgLnS, avgBS);
         }
     }
     /**
@@ -37,14 +39,31 @@ public class Compare {
         return data;
     }
 
-    public static int linearSearch(int[] data) {
+    public static int linearSearch(int[] data, int num) {
 
         return 0;
     }
 
-    public static int binarySearch(int[] data) {
+    public static int binarySearch(int[] data, int num) {
 
 
         return 0;
+    }
+
+    /**
+     * Selects a random int from an array
+     * @param data an array of integers
+     * @return an integer which matches one from the data array
+     */
+    public static int randomSearch(int[] data) {
+        
+        int random = 1 + (int)(Math.random() * 1000000);
+
+        for (int i = 0; i < data.length; i++) {
+            if (random == data[i])
+                return random;
+        }
+
+        return randomSearch(data);
     }
 }
