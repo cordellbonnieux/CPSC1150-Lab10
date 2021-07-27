e5f185a1fcfc8b17c85c200448f76ac98bee2d22
Cordell <cordellbonnieux@gmail.com>
2021-07-26 21:29:17 -0700

    Removed randomSearch() and replaced it with a single line.

--- Compare.java
+++ Compare.java
@@ -8,18 +8,20 @@ public class Compare {
 
         for (int size=10; size <1000000; size*=10) {
 
-            int [] list = genArray(size); 
+            int[] list = genArray(size); 
 
-            int num = randomSearch(list);
+            int num = list[(int)(Math.random() * list.length)];
 
-            int avgLnS = linearSearch(list, num);
+            System.out.println("start: " + num + "...end");
 
-            int avgBS = binarySearch(list, num);
+            //int avgLnS = linearSearch(list, num);
+
+            //int avgBS = binarySearch(list, num);
 
             // develop the code to find out the average comparisons for
             // searching a target in the list using linear search and binary search
 
-            System.out.printf("%-10d%-20.2f%-20.2f\n", size, avgLnS, avgBS);
+            //System.out.printf("%-10d%-20.2f%-20.2f\n", size, avgLnS, avgBS);
         }
     }
     /**
@@ -48,22 +50,5 @@ public class Compare {
 
 
         return 0;
-    }
-
-    /**
-     * Selects a random int from an array
-     * @param data an array of integers
-     * @return an integer which matches one from the data array
-     */
-    public static int randomSearch(int[] data) {
-        
-        int random = 1 + (int)(Math.random() * 1000000);
-
-        for (int i = 0; i < data.length; i++) {
-            if (random == data[i])
-                return random;
-        }
-
-        return randomSearch(data);
     }
 }
