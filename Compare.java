4724b8ccd51fdffb60124b35f1e897aae3e06904
Cordell <cordellbonnieux@gmail.com>
2021-07-26 19:30:40 -0700

    Boilerplate for project added.

---
+++ Compare.java
@@ -0,0 +1,19 @@
+public class Compare {
+    public static void main (String[] args) {
+
+        System.out.printf("\n%10s%-25s\n", " ", "Average Number of Comparisons");
+        System.out.printf("%-10s%12s%20s\n", "n", "Linear Search", "Binary Search");
+        System.out.println("-------------------------------------------");
+
+        for (int size=10; size <1000000; size*=10) {
+        int [] list = genArray(size); //generates an array of random
+        //integers with the given size.
+        
+        // develop the code to find out the average comparisons for
+        // searching a target in the list using linear search and binary search
+        // To sort your data use Arrays.sort(list)
+
+        System.out.printf "%-10d%-20.2f%-20.2f\n", size, avgLnS, avgBS);
+        }
+    }
+}
